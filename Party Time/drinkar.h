611a61a6b8ac40b571e7459e38539cccfa209278
rattai12 <67591655+rattai12@users.noreply.github.com>
2023-05-29 17:28:53 +0200

    Update drinkar.h

--- Party Time/drinkar.h
+++ Party Time/drinkar.h
@@ -4,26 +4,30 @@
 #include <windows.h>
 using namespace std;
 
-class Ingredients {
+class Ingredient {
 public:
-	vector <Ingredients> priceList;
-	string ingredient;
-	int price;
+    string ingredient;
+    int price;
+    vector <Ingredient> priceList;
+    Ingredient(const string& ingredientName, int price) : ingredient(ingredientName), price(price) {}
+    Ingredient() : ingredient(""), price(0) {}
 
-	void readPrice();
-	void showPricelist();
+    void readPrice();
+    void showPricelist();
 };
 
-class Drink: public Ingredients{
+class Drink {
 public:
-	vector<Drink>drinklista;
-	int cl;
-	string name;
+    string name;
+    
+    vector<Ingredient> ingredientList;
+    vector<Drink> drinkList;
+    Drink(const string& drinkName, const vector<Ingredient>& ingredients) : name(drinkName), ingredientList(ingredients) {}
+    Drink() : name(""), ingredientList(vector<Ingredient>()) {}
 
-	
-	Drink(Ingredients ingredient, Drink name, int amount){} //konstruktor för drinkar
-	Drink() : cl(0), name("") {}
-	void blandaDrink();
-	void readDrinks();
-	void drinkRead(string filnamn);
+    void blendDrink();
+    void readDrinks();
+    void drinkRead(string filnamn);
+    void showDrinks();
+    void showSpecificDrink(int number);
 };
